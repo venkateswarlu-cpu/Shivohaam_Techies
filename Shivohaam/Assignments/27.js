1c2c677d7ed982616163f223be28edb243b4f460
venkateswarlu Ambati <147947744+venkateswarlu-cpu@users.noreply.github.com>
2026-01-27 17:51:59 +0530

    i add some files

---
+++ Shivohaam/Assignments/27.js
@@ -0,0 +1,29 @@
+const ele = document.getElementById("4");
+ele.addEventListener("click", () => {
+    alert("Clicked!");
+    const ele1 = document.getElementById("3");
+    ele1.style.color = "blue";
+    const ele2 = document.getElementById("list-ele");
+    ele2.style.color = "#33cc33";
+    ele2.style.backgroundColor="#e6eeff"
+    const head = document.getElementById("shiva");
+    head.textContent="devRathod Online Classes";
+    const bod=document.getElementById("body");
+bod.style.background = "#ffe6f2";
+});
+const element = document.createElement("h3");
+element.textContent="Ambati Venkateswarlu";
+const bod = document.getElementById("body").appendChild(element);
+
+
+const subject = document.getElementById("sub");
+subject.style.marginLeft="22px";
+subject.style.fontFamily="Palatino Linotype";
+
+const head = document.getElementById("ss");
+head.style.marginLeft="22px";
+head.style.fontFamily="Pacifico";
+head.style.color="#8f00b3";
+
+
+
