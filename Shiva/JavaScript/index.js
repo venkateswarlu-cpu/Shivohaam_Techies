a8516e9f8d0ed794587dfdde87c95194bf276e96
venkateswarlu Ambati <147947744+venkateswarlu-cpu@users.noreply.github.com>
2026-01-29 18:10:28 +0530

    Update index.js
    29

--- Shiva/JavaScript/index.js
+++ Shiva/JavaScript/index.js
@@ -1,2 +1,40 @@
 let name = "Shiva";
 console.log("Hello, " + name + "!");
+
+let name = "Ambati Venkateswarlu  ";
+let email = "ambativenkateswarlu0@gmail.com";
+
+let course = "javascript development";
+
+let trimmedName = name.trim();
+let upperName = trimmedName.toUpperCase();
+
+console.log(trimmedName);
+console.log(upperName);
+
+let ind=name.indexOf("Venkateswarlu");
+let lowerEmail = email.toLowerCase();
+
+let UpperEmail = email.toUpperCase();
+let inc=course.includes("javascript");
+
+console.log(lowerEmail);
+console.log(ind);
+console.log(inc);
+console.log(UpperEmail);
+
+let validEmail = lowerEmail.includes("@") && lowerEmail.endsWith(".com");
+
+let courseTitle = course.charAt(0).toUpperCase() + course.slice(1);
+
+let nameLength = trimmedName.length;
+
+let shortName = trimmedName.substring(0, 6);
+let sliceName=name.split("/");
+
+
+console.log(validEmail);
+console.log(courseTitle);
+console.log(nameLength);
+console.log(shortName);
+console.log(sliceName);
